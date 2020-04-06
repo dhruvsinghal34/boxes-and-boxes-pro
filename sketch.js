a635f003393d826eb6e1d80fcf413d0c48b310a4
dhruvsinghal34 <62337112+dhruvsinghal34@users.noreply.github.com>
2020-04-06 15:19:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup(){
+    createCanvas(400,400);
+   }
+function draw(){
+    background("black");
+    rectMode(CENTER);
+    rect(200,200,250,250);
+    rect(200,200,200,200);
+    rect(200,200,150,150);
+    rect(200,200,100,100);
+    rect(200,200,50,50);
+    drawSprites();
+}
+
